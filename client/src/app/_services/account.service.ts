a1875fa9ab49cbe06c7dd9db81c34b390199c3d9
nguyen <125894255+glexiv808@users.noreply.github.com>
2024-03-04 22:07:11 +0700

    Half of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs/operators';
 import { ReplaySubject } from 'rxjs';
-// import { NgIf } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:7021/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
